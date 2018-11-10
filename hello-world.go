9832d5fb90f85ae385d7f94869b21c47e662ce1b
Matthew Lapworth <matthewl@bit-shift.net>
2018-11-10 20:07:24 +0000

    Playing with logging levels
    Signed-off-by: Matthew Lapworth <matthewl@bit-shift.net>

--- hello-world.go
+++ hello-world.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	"fmt" 
-	"os" 
+	"fmt"
+	"os"
+
 	log "github.com/sirupsen/logrus"
 )
 
-func init(){
+func init() {
 	// Log as JSON instead of the default ASCII formatter.
 	log.SetFormatter(&log.JSONFormatter{})
 
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
 	log.SetOutput(os.Stdout)
-  
+
 	// Only log the warning severity or above.
-	log.SetLevel(log.InfoLevel)
+	log.SetLevel(log.WarnLevel)
 }
 
 func main() {
 	fmt.Printf("hello, world\n")
 	log.WithFields(log.Fields{
 		"animal": "walrus",
-	  }).Info("A walrus appears")
+	}).Info("A walrus appears")
+
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 122,
+	}).Warn("The group's number increased tremendously!")
 }
